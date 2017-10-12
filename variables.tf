f22223b06bd116447c164fd4d26bf56046e0d6e6
Tim Hartmann <tfhartmann@gmail.com>
2017-10-12 16:18:16 -0400

    Working Consul RDS Lambda

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "env" {}
+variable "subnets" {
+  type        = "list"
+  description = "List of VPC Subnets IDs used to do lambdas"
+}
+
+variable "rds_sg" {
+  type        = "list"
+  description = "List of Security Groups ID's to use for consulRdsCreateService lambda"
+}
+
+variable "rds_vpc_ids" {
+  type    = "list"
+  default = []
+  description = "List of VPC ID's the consulRdsCreateService lambda will attempt to discover RDS instances in. Defaults empty array"
+}
+
+variable "vpc_id" {}
