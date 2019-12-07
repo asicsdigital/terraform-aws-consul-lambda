3f06a88b912ae0c87e81369da151757b293bf226
Myles Steinhauser <myles.steinhauser@asics.com>
2019-12-07 17:15:44 -0500

    Fix drift detection with full path vs relative path for consulRdsCreateService.zip

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ data "template_file" "json_config" {
   template = <<INPUT
 { "detail": {"vpc_ids": $${rds_vpc_ids} }}
 INPUT
+}  
+
+data "null_data_source" "lambda_file" {
+  inputs {
+    filename = "${substr("${path.module}/files/rds/consulRdsCreateService.zip", length(path.cwd) + 1, -1)}"
+  }
 }
 
 resource "aws_iam_role" "consul_rds" {
@@ -61,11 +67,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
 }
 
 resource "aws_lambda_function" "consulRdsCreateService" {
-  filename         = "${path.module}/files/rds/consulRdsCreateService.zip"
-  function_name    = "consulRdsCreateService-${var.env}"                                            # env var
+  filename         = "${data.null_data_source.lambda_file.outputs.filename}"
+  function_name    = "consulRdsCreateService-${var.env}"
   role             = "${aws_iam_role.consul_rds.arn}"
   handler          = "consulRdsCreateService.lambda_handler"
-  source_code_hash = "${base64sha256(file("${path.module}/files/rds/consulRdsCreateService.zip"))}"
+  source_code_hash = "${base64sha256(file("${data.null_data_source.lambda_file.outputs.filename}"))}"
   runtime          = "python2.7"
   timeout          = "60"
 
